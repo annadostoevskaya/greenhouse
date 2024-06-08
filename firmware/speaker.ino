264137241d978a9364001b8b3aa53125466375cb
Anna Dostoevskaya <iwantknow.aboutjt68h43@gmail.com>
2024-06-08 23:25:27 +0500

    fix: hardcoded port

--- firmware/speaker.ino
+++ firmware/speaker.ino
@@ -3,7 +3,7 @@ const uint8_t g_LED_pin = 8;
 void setup_speaker()
 {
   pinMode(g_LED_pin, OUTPUT);
-  digitalWrite(8, HIGH); // Disable
+  digitalWrite(g_LED_pin, HIGH); // Disable
 }
 
 void speaker(uint32_t interval)
